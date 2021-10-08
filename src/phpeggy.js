b03ffd4e4c27e1f4466ee66e39ecbdaf92360b4c
Marcel Bolten <mail@marcelbolten.de>
2021-10-07 18:47:27 -0700

    update package.json, no tabs

--- src/phpeggy.js
+++ src/phpeggy.js
@@ -23,13 +23,13 @@
  */
 
 exports.use = function(config, options){
-    config.passes.generate = [
-      require("./passes/generate-bytecode-php"),
-      require("./passes/generate-php")
-    ];
-    options.output = "source";
-    if (options.phpeggy) options.phpeggy = options.phppegjs;
-    if (!options.phpeggy) options.phpeggy = {};
-    if (options.phpeggy.parserNamespace === undefined) options.phpeggy.parserNamespace = 'PHPeggy';
-    if (options.phpeggy.parserClassName === undefined) options.phpeggy.parserClassName = 'Parser';
+  config.passes.generate = [
+    require("./passes/generate-bytecode-php"),
+    require("./passes/generate-php")
+  ];
+  options.output = "source";
+  if (options.phpeggy) options.phpeggy = options.phppegjs;
+  if (!options.phpeggy) options.phpeggy = {};
+  if (options.phpeggy.parserNamespace === undefined) options.phpeggy.parserNamespace = 'PHPeggy';
+  if (options.phpeggy.parserClassName === undefined) options.phpeggy.parserClassName = 'Parser';
 };
--- test/grammars.js
+++ test/grammars.js
@@ -9,193 +9,193 @@ const peggy = require( 'peggy' );
 const phpeggy = require( '../src/phpeggy' );
 
 function getUniqueBasenames( array ) {
-	return array
-		// Strip extensions
-		.map( filename => filename.replace( /\..+$/, '' ) )
-		// Filter to unique entries: https://stackoverflow.com/a/14438954
-		.filter( ( value, index, array ) => array.indexOf( value ) === index );
+  return array
+    // Strip extensions
+    .map( filename => filename.replace( /\..+$/, '' ) )
+    // Filter to unique entries: https://stackoverflow.com/a/14438954
+    .filter( ( value, index, array ) => array.indexOf( value ) === index );
 }
 
 function fixtureFilePath( filename ) {
-	if ( Array.isArray( filename ) ) {
-		return path.join.apply(
-			path,
-			[ __dirname, 'fixtures' ].concat( filename )
-		);
-	} else {
-		return path.join( __dirname, 'fixtures', filename );
-	}
+  if ( Array.isArray( filename ) ) {
+    return path.join.apply(
+      path,
+      [ __dirname, 'fixtures' ].concat( filename )
+    );
+  } else {
+    return path.join( __dirname, 'fixtures', filename );
+  }
 }
 
 function runPhp( args, stdin ) {
-	const result = cp.spawnSync( 'php', args, {
-		input: stdin || null,
-		encoding: 'utf8'
-	} );
-	if ( result.error ) {
-		throw result.error;
-	}
-	if ( result.status ) {
-		console.log( {
-			stderr: result.stderr,
-			stdout: result.stdout,
-		} );
-		throw new Error(
-			'Non-zero exit code from PHP: ' + result.status
-		);
-	}
-	return result;
+  const result = cp.spawnSync( 'php', args, {
+    input: stdin || null,
+    encoding: 'utf8'
+  } );
+  if ( result.error ) {
+    throw result.error;
+  }
+  if ( result.status ) {
+    console.log( {
+      stderr: result.stderr,
+      stdout: result.stdout,
+    } );
+    throw new Error(
+      'Non-zero exit code from PHP: ' + result.status
+    );
+  }
+  return result;
 }
 
 console.log( 'Determining version of PHP command-line executable...' );
 const result = runPhp( [ '--version' ] );
 const match = result.stdout.match( /^PHP (\d+)\.(\d+)(\.[^ ]+) / );
 if ( ! match ) {
-	throw new Error( 'Unable to determine PHP version.' );
+  throw new Error( 'Unable to determine PHP version.' );
 }
 console.log( 'PHP version: ' + match[ 0 ].trim() );
 const major = +match[ 1 ];
 const minor = +match[ 2 ];
 if ( major < 5 || ( major === 5 && minor < 2 ) ) {
-	throw new Error(
-		'This library requires at least PHP 5.2.  (Why so old?)'
-	);
+  throw new Error(
+    'This library requires at least PHP 5.2.  (Why so old?)'
+  );
 }
 const isPHP52 = ( major === 5 && minor === 2 );
 console.log(
-	'(Running tests in %s mode)',
-	isPHP52 ? 'PHP 5.2' : 'modern PHP'
+  '(Running tests in %s mode)',
+  isPHP52 ? 'PHP 5.2' : 'modern PHP'
 );
 
 function getPHPParserTestCode( parser, input ) {
-	return parser + `
+  return parser + `
 
 $input = base64_decode( '${ new Buffer( input ).toString( 'base64' ) }' );
 
 try {
-	$parser = new ${ isPHP52 ? 'php52_compat_Parser' : 'Parser' };
-	$result = $parser->parse( $input );
-	echo json_encode( $result );
+  $parser = new ${ isPHP52 ? 'php52_compat_Parser' : 'Parser' };
+  $result = $parser->parse( $input );
+  echo json_encode( $result );
 } catch ( ${ isPHP52 ? 'php52_compat_SyntaxError' : 'SyntaxError' } $ex ) {
-	echo json_encode( array(
-		'error' => array(
-			'message'  => $ex->getMessage(),
-			'expected' => $ex->expected,
-			'found'    => $ex->found,
-			'line'     => $ex->grammarLine,
-			'column'   => $ex->grammarColumn,
-			'offset'   => $ex->grammarOffset,
-		),
-	) );
+  echo json_encode( array(
+    'error' => array(
+      'message'  => $ex->getMessage(),
+      'expected' => $ex->expected,
+      'found'    => $ex->found,
+      'line'     => $ex->grammarLine,
+      'column'   => $ex->grammarColumn,
+      'offset'   => $ex->grammarOffset,
+    ),
+  ) );
 }
 `;
 }
 
 const grammarNames = getUniqueBasenames(
-	fs.readdirSync( path.join( __dirname, 'fixtures' ) )
+  fs.readdirSync( path.join( __dirname, 'fixtures' ) )
 );
 
 grammarNames.forEach( grammarName => {
-	describe( 'Example grammar ' + grammarName, () => {
-		let phpActual;
+  describe( 'Example grammar ' + grammarName, () => {
+    let phpActual;
 
-		it( 'generates the expected PHP code', () => {
-			const grammar = fs.readFileSync(
-				fixtureFilePath( grammarName + '.pegjs' ),
-				'utf8'
-			);
+    it( 'generates the expected PHP code', () => {
+      const grammar = fs.readFileSync(
+        fixtureFilePath( grammarName + '.pegjs' ),
+        'utf8'
+      );
 
-			const peggyOptions = {
-				plugins: [ phpeggy ]
-			};
-			let extraOptions = {};
-			try {
-				extraOptions = JSON.parse( fs.readFileSync(
-					fixtureFilePath( grammarName + '.options.json' ),
-					'utf8'
-				) );
-			} catch ( err ) { }
-			for ( const key in extraOptions ) {
-				peggyOptions[ key ] = extraOptions[ key ];
-			}
-			if ( isPHP52 ) {
-				peggyOptions.phpeggy = peggyOptions.phpeggy || {};
-				peggyOptions.phpeggy.parserNamespace = null;
-				peggyOptions.phpeggy.parserGlobalNamePrefix = 'php52_compat_';
-			}
+      const peggyOptions = {
+        plugins: [ phpeggy ]
+      };
+      let extraOptions = {};
+      try {
+        extraOptions = JSON.parse( fs.readFileSync(
+          fixtureFilePath( grammarName + '.options.json' ),
+          'utf8'
+        ) );
+      } catch ( err ) { }
+      for ( const key in extraOptions ) {
+        peggyOptions[ key ] = extraOptions[ key ];
+      }
+      if ( isPHP52 ) {
+        peggyOptions.phpeggy = peggyOptions.phpeggy || {};
+        peggyOptions.phpeggy.parserNamespace = null;
+        peggyOptions.phpeggy.parserGlobalNamePrefix = 'php52_compat_';
+      }
 
-			try {
-				phpActual = peggy.generate( grammar, peggyOptions );
-			} catch ( err ) {
-				phpActual = util.format(
-					'<?php /*\nERROR GENERATING PARSER:\n\n%s\n\n*/\n',
-					err.message
-				);
-			}
+      try {
+        phpActual = peggy.generate( grammar, peggyOptions );
+      } catch ( err ) {
+        phpActual = util.format(
+          '<?php /*\nERROR GENERATING PARSER:\n\n%s\n\n*/\n',
+          err.message
+        );
+      }
 
-			const phpExpectedPath = fixtureFilePath(
-				grammarName + ( isPHP52 ? '.php52.php' : '.php' )
-			);
-			if (
-				process.env.GENERATE_MISSING_FIXTURES &&
-				! fs.existsSync( phpExpectedPath )
-			) {
-				fs.writeFileSync( phpExpectedPath, phpActual );
-			}
-			const phpExpected = fs.readFileSync(
-				phpExpectedPath,
-				'utf8'
-			);
-			expect( phpActual ).to.eql( phpExpected );
-		} );
+      const phpExpectedPath = fixtureFilePath(
+        grammarName + ( isPHP52 ? '.php52.php' : '.php' )
+      );
+      if (
+        process.env.GENERATE_MISSING_FIXTURES &&
+        ! fs.existsSync( phpExpectedPath )
+      ) {
+        fs.writeFileSync( phpExpectedPath, phpActual );
+      }
+      const phpExpected = fs.readFileSync(
+        phpExpectedPath,
+        'utf8'
+      );
+      expect( phpActual ).to.eql( phpExpected );
+    } );
 
-		let testNames = [];
-		try {
-			const stats = fs.statSync( fixtureFilePath( grammarName ) );
-			if ( stats.isDirectory() ) {
-				testNames = getUniqueBasenames(
-					fs.readdirSync( fixtureFilePath( grammarName ) )
-				);
-			}
-		} catch ( err ) { }
+    let testNames = [];
+    try {
+      const stats = fs.statSync( fixtureFilePath( grammarName ) );
+      if ( stats.isDirectory() ) {
+        testNames = getUniqueBasenames(
+          fs.readdirSync( fixtureFilePath( grammarName ) )
+        );
+      }
+    } catch ( err ) { }
 
-		testNames.forEach( testName => {
-			it( 'generates the expected output for test case ' + testName, () => {
-				const input = fs.readFileSync(
-					fixtureFilePath( [ grammarName, testName + '.txt' ] ),
-					'utf8'
-				);
+    testNames.forEach( testName => {
+      it( 'generates the expected output for test case ' + testName, () => {
+        const input = fs.readFileSync(
+          fixtureFilePath( [ grammarName, testName + '.txt' ] ),
+          'utf8'
+        );
 
-				const result = runPhp( [], getPHPParserTestCode( phpActual, input ) );
-				expect( result.stderr ).to.eql(
-					'',
-					'Received messages from PHP stderr'
-				);
-				let outputActual;
-				try {
-					outputActual = JSON.parse( result.stdout );
-				} catch ( err ) {
-					throw new Error( 'JSON.parse failed:  ' + result.stdout );
-				}
+        const result = runPhp( [], getPHPParserTestCode( phpActual, input ) );
+        expect( result.stderr ).to.eql(
+          '',
+          'Received messages from PHP stderr'
+        );
+        let outputActual;
+        try {
+          outputActual = JSON.parse( result.stdout );
+        } catch ( err ) {
+          throw new Error( 'JSON.parse failed:  ' + result.stdout );
+        }
 
-				const outputExpectedPath =
-					fixtureFilePath( [ grammarName, testName + '.json' ] );
-				if (
-					process.env.GENERATE_MISSING_FIXTURES &&
-					! fs.existsSync( outputExpectedPath )
-				) {
-					fs.writeFileSync(
-						outputExpectedPath,
-						JSON.stringify( outputActual, null, 4 ) + "\n"
-					);
-				}
+        const outputExpectedPath =
+          fixtureFilePath( [ grammarName, testName + '.json' ] );
+        if (
+          process.env.GENERATE_MISSING_FIXTURES &&
+          ! fs.existsSync( outputExpectedPath )
+        ) {
+          fs.writeFileSync(
+            outputExpectedPath,
+            JSON.stringify( outputActual, null, 4 ) + "\n"
+          );
+        }
 
-				const outputExpected = JSON.parse( fs.readFileSync(
-					outputExpectedPath,
-					'utf8'
-				) );
-				expect( outputActual ).to.eql( outputExpected );
-			} );
-		} );
-	} );
+        const outputExpected = JSON.parse( fs.readFileSync(
+          outputExpectedPath,
+          'utf8'
+        ) );
+        expect( outputActual ).to.eql( outputExpected );
+      } );
+    } );
+  } );
 } );
