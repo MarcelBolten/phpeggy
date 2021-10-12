a955c7f9e6a292ac1b7c5c4751dc12925e097b34
Marcel Bolten <mail@marcelbolten.de>
2021-10-11 21:48:33 -0700

    copyright

--- src/passes/generate-bytecode-php.js
+++ src/passes/generate-bytecode-php.js
@@ -5,6 +5,7 @@
  *
  * ------------------------------------------------------------------
  * Copyright (c) 2010-2013 David Majda
+ * Copyright (c) 2014-2021 The PHPeggy AUTHORS
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
