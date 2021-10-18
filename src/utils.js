660baec6bc75250651cd15c3b5833e1187d54ccb
Marcel Bolten <github@marcelbolten.de>
2021-10-17 20:19:54 -0700

    adapt to support peggy 1.2.0

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,7 @@
 "use strict";
 
-const js = require("peggy/lib/compiler/js");
-
 exports.quote = function(str) {
-  return '"' + js.stringEscape(str) + '"';
+  return '"' + stringEscape(str) + '"';
 };
 
 // Matches: /** <?php
@@ -30,3 +28,31 @@ exports.extractPhpCode = function(code) {
   });
   return phpCode ? phpCode : code;
 };
+
+/* eslint-disable no-control-regex */
+
+// Code from peggy
+// It was public on v. 1.1.0 but converted into private in version 1.2.0 (copying here to make phpeggy to work)
+
+function stringEscape(s) {
+  // ECMA-262, 5th ed., 7.8.4: All characters may appear literally in a string
+  // literal except for the closing quote character, backslash, carriage
+  // return, line separator, paragraph separator, and line feed. Any character
+  // may appear in the form of an escape sequence.
+  //
+  // For portability, we also escape all control and non-ASCII characters.
+  return s
+    .replace(/\\/g, "\\\\")  // backslash
+    .replace(/"/g, "\\\"")   // closing double quote
+    .replace(/\0/g, "\\0")   // null
+    .replace(/\x08/g, "\\b") // backspace
+    .replace(/\t/g, "\\t")   // horizontal tab
+    .replace(/\n/g, "\\n")   // line feed
+    .replace(/\v/g, "\\v")   // vertical tab
+    .replace(/\f/g, "\\f")   // form feed
+    .replace(/\r/g, "\\r")   // carriage return
+    .replace(/[\x00-\x0F]/g, ch => "\\x0" + hex(ch))
+    .replace(/[\x10-\x1F\x7F-\xFF]/g, ch => "\\x" + hex(ch))
+    .replace(/[\u0100-\u0FFF]/g, ch => "\\u0" + hex(ch))
+    .replace(/[\u1000-\uFFFF]/g, ch => "\\u" + hex(ch));
+}
