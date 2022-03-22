ba332d0ae5d17e20a950a509405c70617198cc57
Marcel Bolten <github@marcelbolten.de>
2022-03-21 23:47:06 -0700

    Copyright date

--- src/passes/generate-bytecode-php.js
+++ src/passes/generate-bytecode-php.js
@@ -8,7 +8,7 @@
  *
  * ------------------------------------------------------------------
  * Copyright (c) 2010-2013 David Majda
- * Copyright (c) 2014-2021 The PHPeggy AUTHORS
+ * Copyright (c) 2014-2022 The PHPeggy AUTHORS
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
--- src/phpeggy.js
+++ src/phpeggy.js
@@ -1,7 +1,7 @@
 /*
  *   The MIT License (MIT)
  *
- *   Copyright (c) 2014-2021 The PHPeggy AUTHORS
+ *   Copyright (c) 2014-2022 The PHPeggy AUTHORS
  *
  *   Permission is hereby granted, free of charge, to any person obtaining a copy
  *   of this software and associated documentation files (the "Software"), to deal
