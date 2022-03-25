3130dbe90be508bb232626390221e47f5cf68b59
Marcel Bolten <github@marcelbolten.de>
2022-03-24 23:53:31 -0700

    import global php functions

--- src/passes/generate-php.js
+++ src/passes/generate-php.js
@@ -627,11 +627,42 @@ module.exports = function(ast, options) {
   ].join("\n"));
 
   if (phpNamespace) {
-    parts.push("namespace " + phpNamespace + ";");
+    parts.push([
+      "namespace " + phpNamespace + ";",
+      "",
+      "use function array_fill;",
+      "use function array_merge;",
+      "use function array_slice;",
+      "use function array_splice;",
+      "use function call_user_func;",
+      "use function class_exists;",
+      "use function count;",
+      "use function floatval;",
+      "use function floor;",
+      "use function function_exists;",
+      "use function hexdec;",
+      "use function html_entity_decode;",
+      "use function implode;",
+      "use function intval;",
+      "use function json_encode;",
+      "use function log10;",
+      ...mbstringAllowed
+        ? ["use function mb_eregi;", "use function mb_regex_encoding;", "use function mb_strlen;", "use function mb_strtolower;"]
+        : [],
+      "use function min;",
+      "use function ord;",
+      "use function preg_match_all;",
+      "use function preg_split;",
+      "use function str_repeat;",
+      "use function str_replace;",
+      "use function strlen;",
+      "use function substr;",
+      "use function usort;",
+      "",
+    ].join("\n"));
   }
 
   parts.push([
-    "",
     "/* BEGIN Useful functions */",
     "/* chr_unicode - get unicode character from its char code */",
     'if (!function_exists("' + phpGlobalNamePrefixOrNamespaceEscaped + 'chr_unicode")) {',
