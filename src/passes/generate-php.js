921fea5b21ca344058dc53db309943a8d24253aa
Marcel Bolten <github@marcelbolten.de>
2022-03-25 22:39:39 -0700

    extract utility functions

--- src/passes/generate-php.js
+++ src/passes/generate-php.js
@@ -30,6 +30,7 @@ const phpeggyVersion = require("../../package.json").version;
 const peggyVersion = require("peggy/package.json").version;
 const dataStorageClasses = require("./generate-php/data-storage-classes");
 const privateMethods = require("./generate-php/private-methods");
+const utilityFunctions = require("./generate-php/utility-functions");
 
 /* Generates parser PHP code. */
 module.exports = function(ast, options) {
@@ -663,83 +664,10 @@ module.exports = function(ast, options) {
     ].join("\n"));
   }
 
-  parts.push([
-    "/* BEGIN Useful functions */",
-    "/* chr_unicode - get unicode character from its char code */",
-    'if (!function_exists("' + phpGlobalNamePrefixOrNamespaceEscaped + 'chr_unicode")) {',
-    "    /** @param float|int $code */",
-    "    function chr_unicode(",
-    "        $code",
-    "    ): string {",
-    '        return html_entity_decode("&#" . (int) $code .";", ENT_QUOTES, "UTF-8");',
-    "    }",
-    "}",
-    "",
-    "/* ord_unicode - get unicode char code from string */",
-    'if (!function_exists("' + phpGlobalNamePrefixOrNamespaceEscaped + 'ord_unicode")) {',
-    "    function ord_unicode(",
-    "        string $character",
-    "    ): int {",
-    "        if (strlen($character) === 1) {",
-    "            return ord($character);",
-    "        }",
-    "        $json = json_encode($character, JSON_THROW_ON_ERROR);",
-    "        $utf16_1 = (int) hexdec(substr($json, 3, 4));",
-    // A character inside the BMP has a JSON representation like "\uXXXX".
-    // A character outside the BMP looks like "\uXXXX\uXXXX".
-    '        if (substr($json, 7, 2) === "\\u") {',
-    // Outside the BMP.  Math from https://stackoverflow.com/a/6240819
-    "            $utf16_2 = (int) hexdec(substr($json, 9, 4));",
-    "            return 0x10000 + (($utf16_1 & 0x3ff) << 10) + ($utf16_2 & 0x3ff);",
-    "        } else {",
-    "            return $utf16_1;",
-    "        }",
-    "    }",
-    "}",
-    "",
-  ].join("\n"));
-
-  if (mbstringAllowed) {
-    parts.push([
-      "/* peg_regex_test - multibyte regex test */",
-      'if (!function_exists("' + phpGlobalNamePrefixOrNamespaceEscaped + 'peg_regex_test")) {',
-      "    function peg_regex_test(",
-      "        string $pattern,",
-      "        string $string",
-      "    ): bool {",
-      '        if (substr($pattern, -1) === "i") {',
-      "            return (bool) mb_eregi(substr($pattern, 1, -2), $string);",
-      "        } else {",
-      "            return (bool) mb_ereg(substr($pattern, 1, -1), $string);",
-      "        }",
-      "    }",
-      "}",
-      "",
-    ].join("\n"));
-  } else {
-    // Case-insensitive character classes are disallowed via passes.check in file
-    // `reportMbstringIncompatibility.js` if the `mbstringAllowed` option is set to false.
-    parts.push([
-      "/* peg_char_class_test - simple character class test */",
-      'if (!function_exists("' + phpGlobalNamePrefixOrNamespaceEscaped + 'peg_char_class_test")) {',
-      "    /** @param array<int, array<int, int>> $class */",
-      "    function peg_char_class_test(",
-      "        array $class,",
-      "        string $character",
-      "    ): bool {",
-      "        $code = ord_unicode($character);",
-      "        foreach ($class as $range) {",
-      "            if ($code >= $range[0] && $code <= $range[1]) {",
-      "                return true;",
-      "            }",
-      "        }",
-      "        return false;",
-      "    }",
-      "}",
-      "/* END Useful functions */",
-      "",
-    ].join("\n"));
-  }
+  parts.push(utilityFunctions(
+    phpGlobalNamePrefixOrNamespaceEscaped,
+    mbstringAllowed
+  ));
 
   if (ast.topLevelInitializer) {
     const topLevelInitializerCode = internalUtils.extractPhpCode(
