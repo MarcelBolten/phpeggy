a04e1bf4d61da52f01d4bedae52d767e9f3c39c2
Marcel Bolten <github@marcelbolten.de>
2023-03-20 12:28:22 -0700

    eslint ignore phpdoc output

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,11 @@ module.exports = {
   root: true,
   extends: "@peggyjs",
   ignorePatterns: [
+    ".phpdoc/",
     "docker/",
     "examples/",
     "node_modules/",
+    "phpdoc/",
     "test/fixtures",
     "vendor/",
   ],
