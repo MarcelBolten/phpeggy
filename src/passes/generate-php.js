MarcelBolten/phpeggy

commit ed110016545ca1ddae27ad6920003eab75005781

wip multi file

src/passes/generate-php.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const asts = require("peggy/lib/compiler/asts");
-const op = require("../opcodes");
 const Stack = require("peggy/lib/compiler/stack");
+const op = require("../opcodes");
 const internalUtils = require("../utils");
 
 // Load static parser parts
@@ -620,15 +620,21 @@ module.exports = function(ast, options) {
   }
 
   // Global initializer
-  if (ast.topLevelInitializer) {
-    const topLevelInitializerCode = internalUtils.extractPhpCode(
-      ast.topLevelInitializer.code.trim()
-    );
-    if (topLevelInitializerCode !== "") {
-      parts.push(
-        topLevelInitializerCode,
-        ""
+  if (ast.topLevelInitializerl) {
+    const topLevel = Array.isArray(ast.topLevelInitializerl)
+      ? ast.topLevelInitializerl
+      : [ast.topLevelInitializerl];
+    // Put library code before code using it.
+    for (const topLevelInitializerl of topLevel.slice().reverse()) {
+      const topLevelInitializerCode = internalUtils.extractPhpCode(
+        topLevelInitializerl.code.trim()
       );
+      if (topLevelInitializerCode !== "") {
+        parts.push(
+          topLevelInitializerCode,
+          ""
+        );
+      }
     }
   }
 
@@ -699,14 +705,19 @@ module.exports = function(ast, options) {
 
   // Grammar-provided methods
   if (ast.initializer) {
-    const initializerCode = internalUtils.extractPhpCode(
-      ast.initializer.code.trim()
-    );
-    if (initializerCode !== "") {
-      parts.push(...[
-        initializerCode,
-        "",
-      ].map(line => indent(4, line)));
+    const astInitializer = Array.isArray(ast.initializer)
+      ? ast.initializer
+      : [ast.initializer];
+    for (const initializer of astInitializer) {
+      const initializerCode = internalUtils.extractPhpCode(
+        initializer.code.trim()
+      );
+      if (initializerCode !== "") {
+        parts.push(...[
+          initializerCode,
+          "",
+        ].map(line => indent(4, line)));
+      }
     }
   }
 

commit 1072baa6032eb4c8776dc6052da959b626f37329

fix typo in variable name introduced in ed11001

src/passes/generate-php.js
@@ -586,14 +586,14 @@ module.exports = function(ast, options) {
   }
 
   // Global initializer
-  if (ast.topLevelInitializerl) {
-    const topLevel = Array.isArray(ast.topLevelInitializerl)
-      ? ast.topLevelInitializerl
-      : [ast.topLevelInitializerl];
+  if (ast.topLevelInitializer) {
+    const topLevel = Array.isArray(ast.topLevelInitializer)
+      ? ast.topLevelInitializer
+      : [ast.topLevelInitializer];
     // Put library code before code using it.
-    for (const topLevelInitializerl of topLevel.slice().reverse()) {
+    for (const topLevelInitializer of topLevel.slice().reverse()) {
       const topLevelInitializerCode = internalUtils.extractPhpCode(
-        topLevelInitializerl.code.trim()
+        topLevelInitializer.code.trim()
       );
       if (topLevelInitializerCode !== "") {
         parts.push(